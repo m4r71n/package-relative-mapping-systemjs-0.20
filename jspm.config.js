9752a25880ed889d848f0204d01bcbf1a3255748
Martin Lepadusch <m4r71n@users.noreply.github.com>
2018-01-15 10:45:29 +0100

    changed package configuration key

--- jspm.config.js
+++ jspm.config.js
@@ -4,7 +4,50 @@ SystemJS.config({
     "npm:": "jspm_packages/npm/"
   },
   browserConfig: {
-    "baseURL": "/"
+    "baseURL": "/",
+    "bundles": {
+      "build.js": [
+        "app.js",
+        "npm:lodash@4.17.4/forEach.js",
+        "npm:lodash@4.17.4.json",
+        "custom/isArray.js",
+        "npm:lodash@4.17.4/_castFunction.js",
+        "npm:lodash@4.17.4/identity.js",
+        "npm:lodash@4.17.4/_baseEach.js",
+        "npm:lodash@4.17.4/_createBaseEach.js",
+        "npm:lodash@4.17.4/isArrayLike.js",
+        "npm:lodash@4.17.4/isLength.js",
+        "npm:lodash@4.17.4/isFunction.js",
+        "npm:lodash@4.17.4/isObject.js",
+        "npm:lodash@4.17.4/_baseGetTag.js",
+        "npm:lodash@4.17.4/_objectToString.js",
+        "npm:lodash@4.17.4/_getRawTag.js",
+        "npm:lodash@4.17.4/_Symbol.js",
+        "npm:lodash@4.17.4/_root.js",
+        "npm:lodash@4.17.4/_freeGlobal.js",
+        "npm:lodash@4.17.4/_baseForOwn.js",
+        "npm:lodash@4.17.4/keys.js",
+        "npm:lodash@4.17.4/_baseKeys.js",
+        "npm:lodash@4.17.4/_nativeKeys.js",
+        "npm:lodash@4.17.4/_overArg.js",
+        "npm:lodash@4.17.4/_isPrototype.js",
+        "npm:lodash@4.17.4/_arrayLikeKeys.js",
+        "npm:lodash@4.17.4/isTypedArray.js",
+        "npm:lodash@4.17.4/_nodeUtil.js",
+        "npm:lodash@4.17.4/_baseUnary.js",
+        "npm:lodash@4.17.4/_baseIsTypedArray.js",
+        "npm:lodash@4.17.4/isObjectLike.js",
+        "npm:lodash@4.17.4/_isIndex.js",
+        "npm:lodash@4.17.4/isBuffer.js",
+        "npm:lodash@4.17.4/stubFalse.js",
+        "npm:lodash@4.17.4/isArguments.js",
+        "npm:lodash@4.17.4/_baseIsArguments.js",
+        "npm:lodash@4.17.4/_baseTimes.js",
+        "npm:lodash@4.17.4/_baseFor.js",
+        "npm:lodash@4.17.4/_createBaseFor.js",
+        "npm:lodash@4.17.4/_arrayEach.js"
+      ]
+    }
   },
   devConfig: {
     "map": {
@@ -41,11 +84,11 @@ SystemJS.config({
   map: {
     "lodash": "npm:lodash@4.17.4"
   },
-  packages: {    
-    "lodash": {
-        "map": {
-            "./isArray.js": "custom/isArray.js"
-        } 
+  packages: {
+    "npm:lodash@4.17.4": {
+      "map": {
+        "./isArray.js": "custom/isArray.js"
+      }
     }
   }
 });
